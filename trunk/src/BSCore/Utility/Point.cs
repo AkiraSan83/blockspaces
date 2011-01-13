0f114779cd4385ec86173712335e3c4b8df59070
klafter719r <klafter719r@23bab13d-fa4c-9e17-1ad7-28440d1133e6>
2011-01-13 05:42:09 +0000

    Made point hashable

--- trunk/src/BSCore/Utility/Point.cs
+++ trunk/src/BSCore/Utility/Point.cs
@@ -29,5 +29,21 @@ namespace JollyBit.BS.Utility
         {
             return (String.Format("({0},{1},{2})", this.X, this.Y, this.Z));
         }
+
+        private const UInt32 FNV_PRIME = 16777619;
+        private const UInt32 OFFSET_BASIS = 2166136261;
+        /// <summary>
+        /// Hahes a xyz coordinate using FNV hash. Look at
+        /// http://isthe.com/chongo/tech/comp/fnv/ for more info on FNV hash.
+        /// </summary>
+        public override int GetHashCode()
+        {
+            //hash = offset_basis
+            //for each octet_of_data to be hashed
+            //    hash = hash xor octet_of_data
+            //    hash = hash * FNV_prime
+            //return hash
+            return (int)((((((OFFSET_BASIS ^ (UInt32)X) * FNV_PRIME) ^ (UInt32)Y) * FNV_PRIME) ^ (UInt32)Z) * FNV_PRIME);
+        }
     }
 }
