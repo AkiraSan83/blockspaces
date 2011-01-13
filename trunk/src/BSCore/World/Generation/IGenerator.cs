5723d0ae8146febe33dcbf852d26b3646eb2d912
klafter719r <klafter719r@23bab13d-fa4c-9e17-1ad7-28440d1133e6>
2011-01-13 03:37:46 +0000

    1) Added code to generate a sphere world

---
+++ trunk/src/BSCore/World/Generation/IGenerator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using JollyBit.BS.World;
+using JollyBit.BS.Utility;
+
+namespace JollyBit.BS.World.Generation
+{
+    public interface IGenerator
+    {
+        IBlock GenerateBlock(Point3L location);
+    }
+}
---
+++ trunk/src/BSCore/World/Generation/SphereGenerator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace JollyBit.BS.World.Generation
+{
+    public class SphereGenerator : IGenerator
+    {
+        private long _radiusSquared;
+        public long Radius
+        {
+            get{ return (long)Math.Sqrt(_radiusSquared); }
+            set { _radiusSquared = value * value; }
+        }
+        public IBlock GenerateBlock(Utility.Point3L location)
+        {
+            if (location.X * location.X + location.Y * location.Y + location.Z * location.Z > _radiusSquared)
+            {
+                return new Block();
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
