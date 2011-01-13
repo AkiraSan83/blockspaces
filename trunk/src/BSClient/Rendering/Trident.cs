479416563df2f4388c07586b6314f5ceee039961
klafter719r <klafter719r@23bab13d-fa4c-9e17-1ad7-28440d1133e6>
2011-01-13 02:37:37 +0000

    1) Moved trident to separate class 2) Worked on MapRender.cs code

---
+++ trunk/src/BSClient/Rendering/Trident.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OpenTK;
+using OpenTK.Graphics;
+using OpenTK.Graphics.OpenGL;
+using OpenTK.Platform;
+using System.Drawing;
+
+namespace JollyBit.BS.Rendering
+{
+    public class Trident : IRenderable
+    {
+        public void Render()
+        {
+            GL.Begin(BeginMode.Lines);
+                GL.Color3(Color.Red);
+                GL.Vertex3(0f, 0f, 0f);
+                GL.Vertex3(1f, 0f, 0f);
+                GL.Color3(Color.Green);
+                GL.Vertex3(0f, 0f, 0f);
+                GL.Vertex3(0f, 1f, 0f);
+                GL.Color3(Color.Blue);
+                GL.Vertex3(0f, 0f, 0f);
+                GL.Vertex3(0f, 0f, 1f);
+            GL.End();
+        }
+    }
+}
