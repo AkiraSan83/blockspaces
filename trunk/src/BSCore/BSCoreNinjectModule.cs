31b536bba7ed0b8efe75ed7a970072c8bb6b89f0
klafter719r <klafter719r@23bab13d-fa4c-9e17-1ad7-28440d1133e6>
2011-01-14 18:13:45 +0000

    1) Configured and enabled logging.

--- trunk/src/BSCore/BSCoreNinjectModule.cs
+++ trunk/src/BSCore/BSCoreNinjectModule.cs
@@ -17,6 +17,30 @@ namespace JollyBit.BS
             Bind<IMap>().To<Map>();
             Bind<IGenerator>().To<SphereGenerator>();
             Bind<IChunk>().To<Chunk>();
+
+            //Logging config stuff
+            {
+                NLog.Config.LoggingConfiguration config = new NLog.Config.LoggingConfiguration();
+                //targets
+                NLog.Targets.FileTarget fileTarget = new NLog.Targets.FileTarget();
+                config.AddTarget("file", fileTarget);
+                NLog.Targets.ConsoleTarget consoleTarget = new NLog.Targets.ConsoleTarget();
+                config.AddTarget("console", fileTarget);
+
+                fileTarget.FileName = "${basedir}/log.txt";
+                fileTarget.Layout = "${longdate}|${level}|${message}";
+                consoleTarget.Layout = "${date:format=HH\\:MM\\:ss} ${level} ${message}";
+                //rules
+                NLog.Config.LoggingRule loggingRule;
+                loggingRule = new NLog.Config.LoggingRule("*", NLog.LogLevel.Debug, fileTarget);
+                config.LoggingRules.Add(loggingRule);
+                loggingRule = new NLog.Config.LoggingRule("*", NLog.LogLevel.Debug, consoleTarget);
+                config.LoggingRules.Add(loggingRule);
+                //activate
+                NLog.LogManager.Configuration = config;
+            }
+
+            Kernel.Get<Ninject.Extensions.Logging.ILoggerFactory>().GetCurrentClassLogger().Debug("test");
         }
     }
 }
