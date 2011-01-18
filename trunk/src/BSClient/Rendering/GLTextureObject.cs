a52e3bc124fc160e46da01bdbe23efda0fd810f0
klafter719r <klafter719r@23bab13d-fa4c-9e17-1ad7-28440d1133e6>
2011-01-18 23:24:15 +0000

    1) Added MIPMAPPING!!

--- trunk/src/BSClient/Rendering/GLTextureObject.cs
+++ trunk/src/BSClient/Rendering/GLTextureObject.cs
@@ -13,19 +13,48 @@ namespace JollyBit.BS.Rendering
     public class GLTextureObject : IDisposable, IRenderable
     {
         private int _id;
-        public GLTextureObject(Bitmap bitmap)
+        public GLTextureObject(Bitmap bitmap) : this(bitmap, 0) { }
+        public GLTextureObject(Bitmap bitmap, int numMipMapLevels)
         {
             _id = GL.GenTexture();
             GL.BindTexture(TextureTarget.Texture2D, _id);
-            BitmapData data = bitmap.LockBits(new System.Drawing.Rectangle(0, 0, bitmap.Width, bitmap.Height),
-                ImageLockMode.ReadOnly, System.Drawing.Imaging.PixelFormat.Format32bppArgb);
-            GL.TexImage2D(TextureTarget.Texture2D, 0, PixelInternalFormat.Rgba, data.Width, data.Height, 0,
-                OpenTK.Graphics.OpenGL.PixelFormat.Bgra, PixelType.UnsignedByte, data.Scan0);
-            bitmap.UnlockBits(data);
+            GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureBaseLevel, 0);
+            GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMaxLevel, numMipMapLevels - 1);
+            Size currentSize = new Size(bitmap.Width, bitmap.Height);
+            Bitmap currentBitmap = new Bitmap(bitmap);
+            for (int i = 0; i < numMipMapLevels; i++)
+            {
+                //Load currentBitmap
+                BitmapData currentData = currentBitmap.LockBits(new System.Drawing.Rectangle(0, 0, currentBitmap.Width, currentBitmap.Height),
+                    ImageLockMode.ReadOnly, System.Drawing.Imaging.PixelFormat.Format32bppArgb);
+                GL.TexImage2D(TextureTarget.Texture2D, i, PixelInternalFormat.Rgba, currentSize.Width, currentSize.Height, 0,
+                    OpenTK.Graphics.OpenGL.PixelFormat.Bgra, PixelType.UnsignedByte, currentData.Scan0);
+                currentBitmap.UnlockBits(currentData);
+                //Prepare for next iteration
+                currentSize = new Size(currentSize.Width / 2, currentSize.Height / 2);
+                Bitmap tempBitmap = scaleBitmap(currentBitmap, currentSize);
+                currentBitmap.Dispose();
+                currentBitmap = tempBitmap;
+            }
+            currentBitmap.Dispose();
 
-            GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMinFilter, (int)TextureMinFilter.Linear);
+            GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMinFilter, (int)TextureMinFilter.NearestMipmapLinear);
             GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMagFilter, (int)TextureMagFilter.Linear);
         }
+
+        private static Bitmap scaleBitmap(Bitmap source, Size outSize)
+        {
+            Bitmap dest = new Bitmap(outSize.Width, outSize.Height);
+            using (Graphics graphics = Graphics.FromImage(dest))
+            {
+                graphics.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.HighQualityBicubic;
+                graphics.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.HighQuality;
+                graphics.DrawImage(source, 0, 0, dest.Width, dest.Height);
+                graphics.FillRegion(new SolidBrush(Color.FromArgb(255 / 4, 255, 0, 0)), new Region(new Rectangle(0, 0, dest.Width, dest.Height)));
+            }
+            return dest;
+        }
+
         protected virtual void Dispose(bool disposing)
         {
             if (GraphicsContext.CurrentContext != null)
