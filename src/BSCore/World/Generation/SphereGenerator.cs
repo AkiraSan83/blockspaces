2856a56d2f49ff21f9fd4e0e53c6964605abdb8a
klafter719r <klafter719r@23bab13d-fa4c-9e17-1ad7-28440d1133e6>
2011-01-19 07:14:25 +0000

    1) Fixed memory issue

--- src/BSCore/World/Generation/SphereGenerator.cs
+++ src/BSCore/World/Generation/SphereGenerator.cs
@@ -13,11 +13,12 @@ namespace JollyBit.BS.World.Generation
             get { return (long)Math.Sqrt(_radiusSquared); }
             set { _radiusSquared = value * value; }
         }
+        private IBlock _block = new Block();
         public IBlock GenerateBlock(Utility.Point3L location)
         {
             if (location.X * location.X + location.Y * location.Y + location.Z * location.Z < _radiusSquared)
             {
-                return new Block();
+                return _block;
             }
             else
             {
