3e350cf3c99ec022b6b3dee97518117f7526cd26
klafter719r <klafter719r@23bab13d-fa4c-9e17-1ad7-28440d1133e6>
2011-01-22 03:59:28 +0000

    1) Started networking stuff

---
+++ src/BSCore/Networking/NetworkConfig.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using JollyBit.BS.Utility;
+using Lidgren.Network;
+
+namespace JollyBit.BS.Core.Networking
+{
+    public class NetworkConfig : IConfigSection
+    {
+        public int Port = 16061;
+
+        public NetPeerConfiguration CreateNetPeerConfiguration()
+        {
+            NetPeerConfiguration config = new NetPeerConfiguration("BlockSpaces");
+            
+            return config;
+        }
+    }
+}
