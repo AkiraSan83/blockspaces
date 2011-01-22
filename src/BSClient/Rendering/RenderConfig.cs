a9bb40c2482c0a7e9a6eef1dcd26eefd2859de8c
eswanson <eswanson@alloscomp.com>
2011-01-22 06:34:10 +0000

    Added RenderConfig.

---
+++ src/BSClient/Rendering/RenderConfig.cs
@@ -0,0 +1,42 @@
+using System;
+using JollyBit.BS.Utility;
+using JollyBit.BS.Rendering;
+
+using JsonExSerializer;
+
+using Ninject;
+using Ninject.Extensions.Logging;
+
+namespace JollyBit.BS.Client.Rendering
+{
+	public class RenderConfig : IConfigSection
+	{
+		private GLState _glState;
+		private ILogger _logger;
+		
+		public RenderConfig()
+		{ 
+			_glState = BSCoreConstants.Kernel.Get<GLState>();
+			_logger = BSCoreConstants.Kernel.Get<ILoggerFactory>().GetLogger(typeof(RenderConfig)); 
+		}
+		
+		public int MaxTextureSizePower = 11;
+		
+		[JsonExIgnore]
+		public int MaxTextureSize {
+			get {
+				int glSizePower;
+				glSizePower = (int)(Math.Log(_glState.MaxSize) / Math.Log(2));
+				if( glSizePower < MaxTextureSizePower ) {
+					_logger.Warn("Texture size too big. Decreased from 2^{0} to 2^{1}.",MaxTextureSizePower,glSizePower);
+					MaxTextureSizePower = glSizePower;
+				}
+				return (int)Math.Pow(2.0, (double)MaxTextureSizePower);
+			}
+			set {
+				MaxTextureSizePower = (int)(Math.Log(value) / Math.Log(2));
+			}
+		}
+	}
+}
+
