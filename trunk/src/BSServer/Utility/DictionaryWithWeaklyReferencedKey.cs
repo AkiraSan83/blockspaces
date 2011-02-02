4d0c8a294851d3cac6bb56853afc04e46914d99e
klafter719r <klafter719r@23bab13d-fa4c-9e17-1ad7-28440d1133e6>
2011-02-02 01:05:57 +0000

    1) Started implementing a database for server 2) Added SQLite and CMake file to build SQLite 3) Added ADO.net provider for SQLite

---
+++ trunk/src/BSServer/Utility/DictionaryWithWeaklyReferencedKey.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace JollyBit.BS.Core.Utility
+{
+    public class DictionaryWithWeaklyReferencedKey<TKEY,TVALUE> : IDictionary<TKEY, TVALUE>
+    {
+        private Dictionary<int, ICollection<KeyValuePair<WeakReference, TVALUE>>> _dict = new Dictionary<int, ICollection<KeyValuePair<WeakReference, TVALUE>>>();
+        public void Add(TKEY key, TVALUE value)
+        {
+            ICollection<KeyValuePair<WeakReference, TVALUE>> refs;
+            KeyValuePair<WeakReference, TVALUE> pair;
+            TVALUE v;
+            if (tryGetValue(key, out refs, out pair, out v))
+            {
+                throw new ArgumentException("An element with the same key already exists in the Dictionary<TKey, TValue>.");
+            }
+            if(refs == null)
+            {
+                refs = new List<KeyValuePair<WeakReference, TVALUE>>();
+                _dict.Add(key.GetHashCode(), refs);
+            }
+            refs.Add(new KeyValuePair<WeakReference, TVALUE>(new WeakReference(key), value));
+        }
+
+        public bool ContainsKey(TKEY key)
+        {
+            TVALUE value;
+            return TryGetValue(key, out value);
+        }
+
+        public ICollection<TKEY> Keys
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public bool Remove(TKEY key)
+        {
+            ICollection<KeyValuePair<WeakReference, TVALUE>> refs;
+            KeyValuePair<WeakReference, TVALUE> pair;
+            TVALUE value;
+            if (tryGetValue(key, out refs, out pair, out value))
+            {
+                refs.Remove(pair);
+                return true;
+            }
+            return false;
+        }
+
+        private bool tryGetValue(TKEY key, out ICollection<KeyValuePair<WeakReference, TVALUE>> collection, out KeyValuePair<WeakReference, TVALUE> pair, out TVALUE value)
+        {
+            ICollection<KeyValuePair<WeakReference, TVALUE>> refs;
+            if (_dict.TryGetValue(key.GetHashCode(), out refs))
+            {
+                foreach (var reference in refs)
+                {
+                    if (!reference.Key.IsAlive) //Remove dead weak refs
+                    {
+                        refs.Remove(reference);
+                    }
+                    else if (Object.Equals(reference.Key.Target, key))
+                    {
+                        value = (TVALUE)reference.Key.Target;
+                        collection = refs;
+                        pair = reference;
+                        return true;
+                    }
+                }
+                if (refs.Count == 0)
+                {
+                    _dict.Remove(key.GetHashCode()); //Remove empty collections
+                }
+            }
+            collection = refs;
+            pair = default(KeyValuePair<WeakReference, TVALUE>);
+            value = default(TVALUE);
+            return false;
+        }
+
+        public bool TryGetValue(TKEY key, out TVALUE value)
+        {
+            ICollection<KeyValuePair<WeakReference, TVALUE>> refs;
+            KeyValuePair<WeakReference, TVALUE> pair;
+            return tryGetValue(key, out refs, out pair, out value);
+        }
+
+        public ICollection<TVALUE> Values
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public TVALUE this[TKEY key]
+        {
+            get
+            {
+                TVALUE value;
+                if (TryGetValue(key, out value)) return value;
+                throw new KeyNotFoundException();
+            }
+            set
+            {
+                ICollection<KeyValuePair<WeakReference, TVALUE>> refs;
+                KeyValuePair<WeakReference, TVALUE> pair;
+                TVALUE v;
+                if (!tryGetValue(key, out refs, out pair, out v))
+                    throw new KeyNotFoundException();
+                refs.Remove(pair);
+                refs.Add(new KeyValuePair<WeakReference, TVALUE>(pair.Key, value));
+            }
+        }
+
+        public void Add(KeyValuePair<TKEY, TVALUE> item)
+        {
+            Add(item.Key, item.Value);
+        }
+
+        public void Clear()
+        {
+            _dict.Clear();
+        }
+
+        public bool Contains(KeyValuePair<TKEY, TVALUE> item)
+        {
+            TVALUE value;
+            if (TryGetValue(item.Key, out value))
+            {
+                return value.Equals(item.Value);
+            }
+            return false;
+        }
+
+        public void CopyTo(KeyValuePair<TKEY, TVALUE>[] array, int arrayIndex)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Count
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public bool IsReadOnly
+        {
+            get { return false; }
+        }
+
+        public bool Remove(KeyValuePair<TKEY, TVALUE> item)
+        {
+            return Remove(item.Key);
+        }
+
+        public IEnumerator<KeyValuePair<TKEY, TVALUE>> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ trunk/src/BSServer/Utility/ObjectDatabase.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using Ninject.Extensions.Logging;
+using System.Runtime.Serialization;
+
+namespace JollyBit.BS.Core.Utility
+{
+    public interface IUniquelyIdentifiable
+    {
+        Guid UniqueId { get; }
+    }
+    public interface IDatabaseRecord<T> : IUniquelyIdentifiable
+    {
+        T Value { get; }
+    }
+    public interface IDatabaseObject<T> : IDatabaseRecord<T>
+    {
+        T Value { get; set; }
+        Guid UniqueId { get; set; }
+    }
+    public interface IObjectDatabase
+    {
+        IEnumerable<IDatabaseRecord<T>> GetAll<T>();
+        IDatabaseRecord<T> Get<T>();
+        IDatabaseRecord<T> Get<T>(Guid uniqueId);
+        IDatabaseRecord<T> Save<T>(T obj);
+        IEnumerable<IDatabaseRecord<T>> SaveAll<T>(IEnumerable<T> obj);
+        Guid GetObjectsUniqueId(object obj);
+    }
+    public class ObjectDatabase : IObjectDatabase, IDisposable
+    {
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+        private bool _disposed = false;
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!_disposed)
+            {
+                _disposed = true;
+            }
+        }
+        ~ObjectDatabase()
+        {
+            Dispose(false);
+        }
+        protected class DatabaseRecord<T> : IDatabaseRecord<T>
+        {
+            private readonly T _value;
+            private readonly Guid _uniqueId;
+            public DatabaseRecord(Guid uniqueId, T value)
+            {
+                _value = value;
+                _uniqueId = uniqueId;
+            }
+            public T Value
+            {
+                get { return _value; }
+            }
+            public Guid UniqueId
+            {
+                get { return _uniqueId; }
+            }
+        }
+
+        public IEnumerable<IDatabaseRecord<T>> GetAll<T>()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IDatabaseRecord<T> Get<T>()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IDatabaseRecord<T> Get<T>(Guid uniqueId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IDatabaseRecord<T> Save<T>(T obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<IDatabaseRecord<T>> SaveAll<T>(IEnumerable<T> obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Guid GetObjectsUniqueId(object obj)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
