4c80b75c099e25fa585785de807ffcf1330f17ba
klafter719r <klafter719r@23bab13d-fa4c-9e17-1ad7-28440d1133e6>
2011-02-02 01:05:57 +0000

    1) Started implementing a database for server 2) Added SQLite and CMake file to build SQLite 3) Added ADO.net provider for SQLite

---
+++ src/BSServer/Utility/DictionaryBase.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace JollyBit.BS.Core.Utility
+{
+    public abstract class DictionaryBase<TKEY, TVALUE> : IDictionary<TKEY, TVALUE>
+    {
+        public abstract void Add(TKEY key, TVALUE value);
+        public virtual bool ContainsKey(TKEY key)
+        {
+            TVALUE value;
+            return TryGetValue(key, out value);
+        }
+        public virtual ICollection<TKEY> Keys
+        {
+            get
+            {
+                return this.Select(i => i.Key).ToList();
+            }
+        }
+        public abstract bool Remove(TKEY key);
+        public abstract bool TryGetValue(TKEY key, out TVALUE value);
+        public virtual ICollection<TVALUE> Values
+        {
+            get
+            {
+                return this.Select(i => i.Value).ToList();
+            }
+        }
+        public virtual TVALUE this[TKEY key]
+        {
+            get
+            {
+                TVALUE value;
+                if(TryGetValue(key, out value)) return value;
+                throw new KeyNotFoundException();
+            }
+            set
+            {
+                setValue(key, value);
+            }
+        }
+        protected abstract void setValue(TKEY key, TVALUE value);
+        public virtual void Add(KeyValuePair<TKEY, TVALUE> item)
+        {
+            Add(item.Key, item.Value);
+        }
+        public abstract void Clear();
+        public virtual bool Contains(KeyValuePair<TKEY, TVALUE> item)
+        {
+            TVALUE value;
+            if (TryGetValue(item.Key, out value) && value.Equals(item.Value))
+            {
+                return value.Equals(item.Value);
+            }
+            return false;
+        }
+        public virtual void CopyTo(KeyValuePair<TKEY, TVALUE>[] array, int arrayIndex)
+        {
+            foreach (var item in this)
+            {
+                array[arrayIndex] = item;
+                arrayIndex++;
+            }
+        }
+        public abstract int Count { get; }
+        public virtual bool IsReadOnly
+        {
+            get { return true; }
+        }
+        public virtual bool Remove(KeyValuePair<TKEY, TVALUE> item)
+        {
+            TVALUE value;
+            if (TryGetValue(item.Key, out value) && value.Equals(item.Value))
+            {
+                Remove(item.Key);
+            }
+            return false;
+        }
+        public abstract IEnumerator<KeyValuePair<TKEY, TVALUE>> GetEnumerator();
+        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ src/BSServer/Utility/DictionaryWithWeaklyReferencedValues.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace JollyBit.BS.Core.Utility
+{
+    public class DictionaryWithWeaklyReferencedValues<TKEY, TVALUE> : DictionaryBase<TKEY, TVALUE>
+    {
+        private IDictionary<TKEY, WeakReference> _dict = new Dictionary<TKEY, WeakReference>();
+        public override void Add(TKEY key, TVALUE value)
+        {
+            _dict.Add(key, new WeakReference(value));
+        }
+
+        public override bool Remove(TKEY key)
+        {
+            TVALUE value;
+            if (TryGetValue(key, out value))
+            {
+                return _dict.Remove(key);
+            }
+            return false;
+        }
+
+        public override bool TryGetValue(TKEY key, out TVALUE value)
+        {
+            WeakReference reference;
+            if (_dict.TryGetValue(key, out reference))
+            {
+                if (reference.IsAlive)
+                {
+                    value = (TVALUE)reference.Target;
+                    return true;
+                }
+                _dict.Remove(key); //Remove dead weak refs
+            }
+            value = default(TVALUE);
+            return false;
+        }
+
+        protected override void setValue(TKEY key, TVALUE value)
+        {
+            _dict[key] = new WeakReference(value);
+        }
+
+        public override void Clear()
+        {
+            _dict.Clear();
+        }
+
+        public override int Count
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public override IEnumerator<KeyValuePair<TKEY, TVALUE>> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
