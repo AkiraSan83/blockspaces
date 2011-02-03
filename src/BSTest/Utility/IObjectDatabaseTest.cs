7ab1436a3b1446540c06ae26fd3bf02460b39bb0
klafter719r <klafter719r@23bab13d-fa4c-9e17-1ad7-28440d1133e6>
2011-02-03 02:19:52 +0000

    1) Added NUnit 2) Added BSTest Project

---
+++ src/BSTest/Utility/IObjectDatabaseTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace Jollybit.BS.Test.Utility
+{
+    [TestFixture]
+    public class ObjectDatabaseTest
+    {
+        [TestFixtureSetUp]
+        public void Setup()
+        {
+            
+        }
+        [TestFixtureTearDown]
+        public void TearDown()
+        {
+
+        }
+    }
+}
