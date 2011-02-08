41ecc25989077b30ccc966eca3f3556fef7cfe3a
klafter719r <klafter719r@23bab13d-fa4c-9e17-1ad7-28440d1133e6>
2011-02-08 18:58:11 +0000

    1) Replaced all DataContractAtrributes with ProtoContractAttributes

--- trunk/src/BSCore/Networking/Messages/InitializationCompleteMessage.cs
+++ trunk/src/BSCore/Networking/Messages/InitializationCompleteMessage.cs
@@ -2,12 +2,12 @@
 using JollyBit.BS.Core.World;
 using JollyBit.BS.Core;
 using JollyBit.BS.Core.Utility;
-using System.Runtime.Serialization;
+using ProtoBuf;
 
 namespace JollyBit.BS.Core.Networking.Messages
 {
     [Message(Lidgren.Network.NetDeliveryMethod.ReliableOrdered, SequenceChannel = SequenceChannels.Initialization)]
-    [DataContract]
+    [ProtoContract]
     public class InitializationCompleteMessage
     {
     }
